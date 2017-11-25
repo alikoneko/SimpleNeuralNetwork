c200690d88a6ccc78f6b04f560fc7b8d745e1798
Alison <alison.neer@gmail.com>
2017-11-25 11:08:47 -0500

    Service Registry

---
+++ ServiceRegistry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNeuralNetwork
+{
+    class ServiceRegistry
+    {
+        private static ServiceRegistry instance;
+        private Random random;
+
+        public static ServiceRegistry GetInstance()
+        {
+            if (null == instance)
+            {
+                instance = new ServiceRegistry();
+            }
+
+            return instance;
+        }
+
+        public ServiceRegistry()
+        {
+            random = new Random();
+        }
+
+        public Random GetRandom()
+        {
+            return random;
+        }
+    }
+}
