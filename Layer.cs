4c65e6f5e9c31876954a065e960c65536944c5d9
Alison <alison.neer@gmail.com>
2017-11-25 11:38:32 -0500

    Layers

---
+++ Layer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNeuralNetwork
+{
+    class Layer
+    {
+        public List<Neuron> Neurons { get; set; }
+        public int NeuronCount
+        {
+            get
+            {
+                return Neurons.Count;
+            }
+        }
+
+        public Layer(int numberOfNeurons)
+        {
+            Neurons = new List<Neuron>(numberOfNeurons);
+
+        }
+    }
+}
